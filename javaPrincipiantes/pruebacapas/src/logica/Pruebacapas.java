d669195c309b80c0905cdbfc997857aebe5ddacd
pablesdev <pablesdev@gmail.com>
2024-06-03 11:02:25 -0300

    practica de modelo de capas básico

--- javaPrincipiantes/pruebacapas/src/logica/Pruebacapas.java
+++ javaPrincipiantes/pruebacapas/src/logica/Pruebacapas.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package logica;
 
-/**
- *
- * @author pables
- */
+import igu.Pantalla;
+
 public class Pruebacapas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Pantalla panta  = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+        
+        
     }
     
 }
