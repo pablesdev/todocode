9a4b7aff066d6e5ac7c63b2c1e1a063f4106aa3a
pablesdev <pablesdev@gmail.com>
2024-12-19 22:50:37 -0300

    avanzando con proyectos iniciales

---
+++ JavaPrincipiantes/Variables/src/variables/Variables.java
@@ -0,0 +1,17 @@
+
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        
+        int edad;
+        double estatura;
+        boolean tieneCarnet;
+        char letra;
+        String direccion;
+        
+        
+    }
+    
+}
